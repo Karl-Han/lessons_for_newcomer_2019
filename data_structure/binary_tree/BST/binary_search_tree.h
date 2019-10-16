01e8323a2a90f3adb49a6bb887be5ae36a9d43ed
Karl Han <karl-han@iwktd.com>
2019-10-16 12:04:58 +0800

    Just started BST.

---
+++ data_structure/binary_tree/BST/binary_search_tree.h
@@ -0,0 +1,12 @@
+#ifndef __BST
+#define __BST
+
+/*
+ * Binary-search-tree property:
+ * Let x be a node in a binary search tree.
+ * * If y is a node in the left subtree of x, then y:key < x:key.
+ * * If y is a node in the right subtree of x, then y:key > x:key.
+ *
+ */
+
+#endif
